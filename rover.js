d39025b9daea44ef627ae149bdaa7ff66a8cc1fb
Marc Byndas <byndasma@gmail.com>
2024-02-26 14:05:15 -0500

    slight refactor

--- rover.js
+++ rover.js
@@ -1,56 +1,53 @@
 // Rover receives a message object to update its key values
 // rover = { position, mode, generatorWatts, receiveMessage() }
 class Rover {
-  constructor(position, mode = "NORMAL", watts = 110) {
-    this.position = position;
-    this.mode = mode; // mode: "LOW_POWER" prevents "MOVE" commands from updating rover position
-    this.generatorWatts = watts;
-  }
-  // updates rover object key values
-  // returns response object with at least two keys: message name & results array
-  receiveMessage(messageObj) {
-    let responseObj = {};
-    responseObj.message = messageObj.name; // message name
-    responseObj.results = [];
+	constructor(position, mode = "NORMAL", watts = 110) {
+		this.position = position;
+		this.mode = mode; // mode: "LOW_POWER" prevents "MOVE" commands from updating rover position
+		this.generatorWatts = watts;
+	}
+	// updates rover object key values
+	// returns response object with at least two keys: message name & results array
+	receiveMessage(messageObj) {
+		const responseObj = {
+			message: messageObj.name, // message name
+			results: [],
+		};
 
-    // handles each command type
-    // populates response.results array with expected command results
-    messageObj.commands.forEach((command) => {
-      if (command.commandType === "MODE_CHANGE") {
-        this.mode = command.value;
-        responseObj.results.push({ completed: true });
-      }
-      // updates rover command completed response to false
-      // if move command occurs during low power mode
-      if (command.commandType === "MOVE") {
-        if (this.mode === "LOW_POWER") {
-          responseObj.results.push({ completed: false });
-        } else {
-          this.position = command.value;
-          responseObj.results.push({
-            completed: true,
-          });
-        }
-      }
-      if (command.commandType === "STATUS_CHECK") {
-        responseObj.results.push({
-          completed: true,
-          roverStatus: {
-            position: this.position,
-            mode: this.mode,
-            generatorWatts: this.watts || 110,
-          },
-        });
-      }
-    });
-    // console.log("RESPSONSEobj:", responseObj);
-    return responseObj;
-  }
+		// handles each command type
+		// populates response.results array with expected command results
+		messageObj.commands.forEach((command) => {
+			if (command.commandType === "MODE_CHANGE") {
+				this.mode = command.value;
+				responseObj.results.push({ completed: true });
+			}
+			// updates rover command completed response to false
+			// if move command occurs during low power mode
+			else if (command.commandType === "MOVE") {
+				if (this.mode === "LOW_POWER") {
+					responseObj.results.push({ completed: false });
+				} else {
+					this.position = command.value;
+					responseObj.results.push({
+						completed: true,
+					});
+				}
+			} else if (command.commandType === "STATUS_CHECK") {
+				responseObj.results.push({
+					completed: true,
+					roverStatus: {
+						position: this.position,
+						mode: this.mode,
+						generatorWatts: this.watts || 110,
+					},
+				});
+			}
+		});
+		// console.log("RESPSONSEobj:", responseObj);
+		return responseObj;
+	}
 }
 
 module.exports = Rover;
 
-/*=================================================================================================
-Question: my GitHub profile only adds a green box to my activity calendar when creating a new repo.
-Is there a way to add an GitHub calendar green box each time thatI push changes to my GitHub repo?
-=================================================================================================*/
+// console.log(JSON.stringify(response, null, 2));
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -3,18 +3,18 @@ const Command = require("../command.js");
 const Message = require("../message.js");
 const Rover = require("../rover.js");
 
-let commands = [
-  new Command("MODE_CHANGE", "LOW_POWER"),
-  new Command("STATUS_CHECK"),
+const commands = [
+	new Command("MODE_CHANGE", "LOW_POWER"),
+	new Command("STATUS_CHECK"),
 ];
 
-let rover = new Rover(98382); // 98382 as arg1 sets rover's position
+const rover = new Rover(98382); // 98382 as arg1 sets rover's position
 // { position: #, mode: '', generatedWatts: # }
 
-let message = new Message("Test message with two commands", commands);
+const message = new Message("Test message with two commands", commands);
 // { name: '', commands: [] }
 
-let response = rover.receiveMessage(message);
+const response = rover.receiveMessage(message);
 // { message: '', results: [] }
 
 /*  EXPECTED ROVER RESPONSE OBJECT:
@@ -28,52 +28,54 @@ let response = rover.receiveMessage(message);
 } */
 
 describe("Rover class", function () {
-  test(`constructor sets position and default values for mode and generatorWatts`, () => {
-    expect(rover.position).toBe(98382);
-    expect(rover.mode).toBe("LOW_POWER");
-    expect(rover.generatorWatts).toBe(110);
-  });
-  test(`response returned by receiveMessage contains the name of the message`, () => {
-    expect(response.message).toBe("Test message with two commands");
-  });
-  test(`response returned by receiveMessage includes two results if two commands are sent in the message`, () => {
-    expect(response.results.length).toBe(message.commands.length);
-  });
-  test(`responds correctly to the status check command`, () => {
-    message.commands.forEach((command, index) => {
-      if (command.commandType === "STATUS_CHECK") {
-        expect(response.results[index].roverStatus.position).toBe(
-          rover.position
-        );
-        expect(response.results[index].roverStatus.mode).toBe(rover.mode);
-        expect(response.results[index].roverStatus.generatorWatts).toBe(
-          rover.generatorWatts
-        );
-      }
-    });
-  });
-  test(`responds correctly to the mode change command`, () => {
-    message.commands.forEach((command, index) => {
-      if (command.commandType === "MODE") {
-        expect(response.results[index].completed).toBe(true);
-        expect(rover.mode).toBe(command.value);
-      }
-    });
-  });
-  test(`responds with a false completed value when attempting to move in LOW_POWER mode`, () => {
-    message.commands.forEach((command, index) => {
-      if (command.commandType === "MOVE") {
-        if (response.results[index].roverStatus.mode === "LOW_POWER") {
-          expect(response.results[index].completed).toBe(false);
-        }
-      }
-    });
-  });
-  test(`responds with the position for the move command`, () => {
-    message.commands.forEach((command) => {
-      if (command.commandType === "MOVE") {
-        expect(rover.position).toBe(command.value);
-      }
-    });
-  });
+	test(`constructor sets position and default values for mode and generatorWatts`, () => {
+		expect(rover.position).toBe(98382);
+		expect(rover.mode).toBe("LOW_POWER");
+		expect(rover.generatorWatts).toBe(110);
+	});
+	test(`response returned by receiveMessage contains the name of the message`, () => {
+		expect(response.message).toBe("Test message with two commands");
+	});
+	test(`response returned by receiveMessage includes two results if two commands are sent in the message`, () => {
+		expect(response.results.length).toBe(message.commands.length);
+	});
+	test(`responds correctly to the status check command`, () => {
+		message.commands.forEach((command, index) => {
+			if (command.commandType === "STATUS_CHECK") {
+				expect(response.results[index].roverStatus.position).toBe(
+					rover.position
+				);
+				expect(response.results[index].roverStatus.mode).toBe(
+					rover.mode
+				);
+				expect(response.results[index].roverStatus.generatorWatts).toBe(
+					rover.generatorWatts
+				);
+			}
+		});
+	});
+	test(`responds correctly to the mode change command`, () => {
+		message.commands.forEach((command, index) => {
+			if (command.commandType === "MODE") {
+				expect(response.results[index].completed).toBe(true);
+				expect(rover.mode).toBe(command.value);
+			}
+		});
+	});
+	test(`responds with a false completed value when attempting to move in LOW_POWER mode`, () => {
+		message.commands.forEach((command, index) => {
+			if (command.commandType === "MOVE") {
+				if (response.results[index].roverStatus.mode === "LOW_POWER") {
+					expect(response.results[index].completed).toBe(false);
+				}
+			}
+		});
+	});
+	test(`responds with the position for the move command`, () => {
+		message.commands.forEach((command) => {
+			if (command.commandType === "MOVE") {
+				expect(rover.position).toBe(command.value);
+			}
+		});
+	});
 });
